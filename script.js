549b58ff2d9ed1b722e38ab88a87b05261dfe8bd
dianareginaojeda-dev <dianareginaojeda@gmail.com>
2026-01-28 09:46:01 -0400

    Add zoom effect on image hover in zoom-container

--- script.js
+++ script.js
@@ -254,3 +254,21 @@ ${cartItems}
   cart = [];
   updateCartModal();
 });
+
+document.querySelectorAll('.zoom-container').forEach(container => {
+  const img = container.querySelector('img');
+
+  container.addEventListener('mousemove', (e) => {
+    const rect = container.getBoundingClientRect();
+    const x = ((e.clientX - rect.left) / rect.width) * 100;
+    const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+    img.style.transformOrigin = `${x}% ${y}%`;
+    img.style.transform = 'scale(2)';
+  });
+
+  container.addEventListener('mouseleave', () => {
+    img.style.transformOrigin = 'center center';
+    img.style.transform = 'scale(1)';
+  });
+});
